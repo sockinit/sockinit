ee3a0d136fd426a1e3d66f2d762cb6d310e8d145
tu6619 <tu6619@my.bristol.ac.uk>
2016-02-23 14:29:41 +0000

    server.js file set up

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
-//var server =
 
-//export the server
 
-function initialise (router, port){
+function initialise (router){
     var server = http.createServer(router).listen(port);
     console.log('server listening on: ' + port);
+    console.log(typeof server);
     return server;
 }
 
 module.exports = {
-    initialise: initialise,
-    port: port
+    initialise: initialise
 };
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,18 +1,20 @@
-//var server =
-
 var tape = require('tape');
 var server = require('../server/server.js');
 
-tape('server successfully exports function initialise, which initialises the server', function(t) {
-    var actual = typeof server;
-    var expected = 'object';
+
+tape('server successfully exports a function', function(t) {
+    var actual = typeof server.initialise;
+    var expected = 'function';
     t.equal(actual, expected, 'initialise is indeed a function');
     t.end();
 });
 
 
-
-
-
-
-//export the server
+tape('calling initialise in the server module returns an object (the server)', function(t){
+    var s = server.initialise();
+    var actual = typeof s;
+    var expected = 'object';
+    t.equal(actual, expected, 'initialise returns an object');
+    t.end();
+    s.close();
+});
