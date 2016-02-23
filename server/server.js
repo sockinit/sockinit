2cf1aae00874d8193c3e204681bcede9140c1e6b
tu6619 <tu6619@my.bristol.ac.uk>
2016-02-23 13:34:07 +0000

    first test and function added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
-
+var http = require('http');
+var port = process.env.PORT || 3000;
 //var server =
 
 //export the server
+
+function initialise (router, port){
+    var server = http.createServer(router).listen(port);
+    console.log('server listening on: ' + port);
+    return server;
+}
+
+module.exports = {
+    initialise: initialise,
+    port: port
+};
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,4 +1,18 @@
-
 //var server =
+
+var tape = require('tape');
+var server = require('../server/server.js');
+
+tape('server successfully exports function initialise, which initialises the server', function(t) {
+    var actual = typeof server;
+    var expected = 'object';
+    t.equal(actual, expected, 'initialise is indeed a function');
+    t.end();
+});
+
+
+
+
+
 
 //export the server
