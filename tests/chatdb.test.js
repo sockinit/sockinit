8012dce227f78ac27dcb5c61f109e75f6801ef5b
virginie <virginietrubiano@gmail.com>
2016-02-25 11:57:26 +0000

    fix retrieveChat function + print message on the client side

--- tests/chatdb.test.js
+++ tests/chatdb.test.js
@@ -6,8 +6,16 @@ var clientMock = redisMock.createClient();
 var redis = require('redis');
 var client = redis.createClient();
 
+var testObj = {
+  userName : 'Norbert',
+  img : 'someimagestring',
+  message : 'hello world',
+  date : '21/01/1990',
+  time : '00:27'
+};
+
 tape('Function addTodb gets response from the database', function(t){
-    chatdb.addToDb(clientMock, 'Norbert', 'someimagestring', 'hello world', '21/01/1990', '00:27', function(reply){
+    chatdb.addToDb(clientMock, testObj, function(reply){
         var expected = 'number';
         var actual = reply;
         t.equal(typeof actual, expected);
@@ -15,11 +23,11 @@ tape('Function addTodb gets response from the database', function(t){
     });
 });
 
-tape('Function retrieveChats retrieve the list chats from the database', function(t){
-    chatdb.retrieveChats(clientMock, function(reply){
-        var expected = '?';
-        var actual = reply;
-        t.equal(actual, expected);
+tape('Function retrieveChats retrieves an array', function(t){
+    chatdb.retrieveChats(client, function(reply){
+        // var expected = 'array';
+        var actual =  reply instanceof Array;
+        t.ok(actual, 'Array is returned');
         t.end();
     });
 });
