f52c02f02e54f1f0641b5a6013966d6760019e5f
ivanmauricio <ivangh@hotmail.co.uk>
2016-02-25 14:41:03 +0000

    returning sockers functionality added

--- client/main.js
+++ client/main.js
@@ -33,3 +33,36 @@ function sendNameToBackEnd(name) {
     request.open("GET", "/username/" + name, true);
     request.send();
 }
+
+document.getElementById("rebutton").addEventListener('click', function(e) {
+    e.preventDefault();
+    var username = document.getElementById('reinput').value;
+    console.log(username);
+    reSendNameToBackEnd(username);
+});
+
+function reSendNameToBackEnd(name) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response, request.responseText);
+            if(request.responseText === 'true') {
+                userName = name;
+                console.log('in refunc user', userName);
+                var successEl = document.createElement('h2');
+                successEl.innerHTML = 'Success, get Socking!';
+                document.body.appendChild(successEl);
+                setTimeout(function(){
+                    document.getElementById('link').click();
+                }, 2000);
+            }
+            else {
+                var sorryEl = document.createElement('h2');
+                sorryEl.innerHTML = 'Sorry, we can\'t find your username. Try again.';
+                document.body.appendChild(sorryEl);
+            }
+        }
+    };
+    request.open("GET", "/username/" + name, true);
+    request.send();
+}
